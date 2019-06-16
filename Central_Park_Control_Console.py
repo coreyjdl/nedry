92e2291f1b89a036adaeb88fb5fe32839f1a92b6
Corey <coreyjdl@gmail.com>
2019-06-16 14:36:40 -0500

    init

---
+++ Central_Park_Control_Console.py
@@ -0,0 +1,21 @@
+import os
+from time import sleep
+
+os.system('cls')
+
+print('Jurassic Park, System Security Interface')
+print('Version 4.0.5, Alpha E')
+print('Ready...')
+input('> ')
+print('access: PERMISSION DENIED.')
+input('> ')
+print('access: PERMISSION DENIED.')
+input('> ')
+os.popen('npm start')
+print('access: PERMISSION DENIED', end='')
+sleep(.4)
+print('....and...')
+for i in range(100):
+    sleep(.2)
+    print('YOU DIDN\'T SAY THE MAGIC WORD!')
+input()
